f50277042314a70ef3fa3cdb566638f492b6b05d
Caitlin Croteau <cjcroteau@gmail.com>
2023-02-06 10:26:35 -0800

    add player script

---
+++ PlayerScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public float playerSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal"); 
+        float deltaY = Input.GetAxis("Vertical");
+
+        Vector3 change = new Vector3(deltaX, deltaY, 0) *playerSpeed* Time.deltaTime;;
+
+        transform.position += change;
+    }
+}
