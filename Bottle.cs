37131121a644dca5ace5b30e035eee16d918bfd3
Colton <coltondavenport@gmail.com>
2023-05-24 11:46:08 -0700

    add bottle collision

--- Bottle.cs
+++ Bottle.cs
@@ -6,6 +6,7 @@ public class Bottle : MonoBehaviour
 {
     Rigidbody2D rb2d;
     public float force = 1f;
+    [SerializeField] private GameObject fire;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +21,11 @@ public class Bottle : MonoBehaviour
             Destroy(this.gameObject);
         }
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+       
+        Instantiate(fire, transform.position, transform.rotation);
+        Destroy(this.gameObject);
     }
 }
